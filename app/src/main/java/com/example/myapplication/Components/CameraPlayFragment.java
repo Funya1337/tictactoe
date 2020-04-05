9dbff1ecd60a706be9596283099b7f368e327257
Funya1337 <funya7654@gmail.com>
2020-04-05 15:03:14 +0300

    feat(cameraPart): done camera part

--- app/src/main/java/com/example/myapplication/Components/CameraPlayFragment.java
+++ app/src/main/java/com/example/myapplication/Components/CameraPlayFragment.java
@@ -1,11 +1,13 @@
 package com.example.myapplication.Components;
 
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,15 +17,16 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.myapplication.Classes.Board;
 import com.example.myapplication.Classes.DataBaseHelper;
+import com.example.myapplication.Classes.DialogNotifier;
 import com.example.myapplication.Classes.ElState;
 import com.example.myapplication.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class CameraPlayFragment extends Fragment {
-    ElState turn = ElState.X;
-    private ElState firstPlayer = ElState.E;
-    ElState winnerCheckVar = ElState.E;
+    private ElState turn = ElState.X;
+    private ElState winnerCheckVar = ElState.E;
     private DataBaseHelper mDatabaseHelper;
     private String lastEl;
     private String preLastEl;
@@ -31,12 +34,25 @@ public class CameraPlayFragment extends Fragment {
 
     final Board newBoard = new Board();
 
-    private void nextTurn() {
+    private void nextTurnForBoard() {
         turn = turn == ElState.X ? ElState.O : ElState.X;
     }
 
-    private String getTurnText() {
-        return turn == ElState.X ? "X" : "O";
+    private void nextTurn()
+    {
+        checkerInFragment = !checkerInFragment;
+    }
+
+    private String getTurnImage() {
+        mDatabaseHelper = new DataBaseHelper(getActivity().getApplicationContext());
+        ArrayList<String> listData = new ArrayList<>();
+        Cursor data1 = mDatabaseHelper.getData();
+        while(data1.moveToNext()){
+            listData.add(data1.getString(1));
+        }
+        lastEl = listData.get(listData.size() - 1);
+        preLastEl = listData.get(listData.size() - 2);
+        return checkerInFragment ? preLastEl : lastEl;
     }
 
     public CameraPlayFragment(boolean checker) {
@@ -44,16 +60,23 @@ public class CameraPlayFragment extends Fragment {
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.camera_play_fragment, container, false);
-        getData();
-        System.out.println(checkerInFragment);
+        final View rootView = inflater.inflate(R.layout.camera_play_fragment, container, false);
+        Button button = rootView.findViewById(R.id.button_reset);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                newBoard.clearBoard();
+                newBoard.print();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.detach(CameraPlayFragment.this).attach(CameraPlayFragment.this).commit();
+            }
+        });
         for (int i = 0; i < newBoard.boardSize; i++) {
             for (int j=0; j< newBoard.boardSize; j++) {
                 final int indexI = i;
                 final int indexJ = j;
                 String buttonId = "button_" + indexI + "_" + indexJ;
-                final Button button = rootView.findViewById(getResources().getIdentifier(buttonId, "id", getActivity().getPackageName()));
-                button.setOnClickListener(new View.OnClickListener() {
+                final ImageView imageView = rootView.findViewById(getResources().getIdentifier(buttonId, "id", getActivity().getPackageName()));
+                imageView.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
                         newBoard.setElement(indexI, indexJ, turn);
                         newBoard.print();
@@ -69,8 +92,12 @@ public class CameraPlayFragment extends Fragment {
                             winnerCheckVar = ElState.N;
                             System.out.println(winnerCheckVar + " IS WINNER");
                         }
-                        button.setText(getTurnText());
+                        Picasso.get()
+                                .load(Uri.parse(getTurnImage()))
+                                .resize(400, 400)
+                                .into(imageView);
                         nextTurn();
+                        nextTurnForBoard();
                         v.setClickable(false);
                         v.setEnabled(false);
                     }
@@ -78,18 +105,5 @@ public class CameraPlayFragment extends Fragment {
             }
         }
         return rootView;
-    }
-    public void getData()
-    {
-        mDatabaseHelper = new DataBaseHelper(getActivity().getApplicationContext());
-        ArrayList<String> listData = new ArrayList<>();
-        Cursor data1 = mDatabaseHelper.getData();
-        while(data1.moveToNext()){
-            listData.add(data1.getString(1));
-        }
-        lastEl = listData.get(listData.size() - 1);
-        preLastEl = listData.get(listData.size() - 2);
-        System.out.println(lastEl);
-        System.out.println(preLastEl);
     }
 }
